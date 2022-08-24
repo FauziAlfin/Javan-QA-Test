ad1d5b4201a591d72861bfb04fa83532ea8671c2
alfinfauzi <alfinfauzi@localhost>
2022-08-25 00:39:56 +0700

    Test Javan

---
+++ Object Repository/Portofolio Bugs/Page_Home  PT Javan Cipta Solusi/a_Portfolio.rs
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Portfolio</name>
+   <tag></tag>
+   <elementGuidId>b7cba470-cef4-4f22-99bb-98ad04d28f9c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@id='mobileMenu']/li[5]/div/ul/li/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>6fbe633c-4ffd-4bba-8af8-48fae727f758</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>rounded-t bg-white hover:bg-gray-100 py-4 px-4 block
+                            whitespace-no-wrap</value>
+      <webElementGuid>dc57230c-fed7-4fc3-8735-eeac50f459b7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/portfolio</value>
+      <webElementGuid>75008434-17f5-4df9-ba12-127e5f769170</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                            
+                                            Portfolio
+                                        </value>
+      <webElementGuid>cde2892f-6c90-4506-872f-e646e3f232bf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;mobileMenu&quot;)/li[@class=&quot;pr-5&quot;]/div[@class=&quot;dropdown inline-block relative&quot;]/ul[@class=&quot;dropdown-menu z-10 shadow-lg rounded-lg fixed hidden text-base text-gray-600 pt-1&quot;]/li[@class=&quot;border-b&quot;]/a[@class=&quot;rounded-t bg-white hover:bg-gray-100 py-4 px-4 block
+                            whitespace-no-wrap&quot;]</value>
+      <webElementGuid>36fa4d08-f55a-4039-9d4a-d5b55e5372f2</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//ul[@id='mobileMenu']/li[5]/div/ul/li/a</value>
+      <webElementGuid>01fc1e3b-239b-4d06-a72f-5fe92b53a980</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Portfolio'])[3]/following::a[1]</value>
+      <webElementGuid>ea8a33ce-cc86-44e3-9962-d85ae48ceda9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Knowledge Center'])[2]/preceding::a[2]</value>
+      <webElementGuid>a6329064-ffae-4803-b9aa-95ba83dcfae4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '/portfolio')])[3]</value>
+      <webElementGuid>0e12ed56-60b3-4a83-8f21-f3f8756c4e17</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[5]/div/ul/li/a</value>
+      <webElementGuid>a9e05ad0-f204-4b90-b33e-6064eb5cefcf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/portfolio' and (text() = '
+                                            
+                                            Portfolio
+                                        ' or . = '
+                                            
+                                            Portfolio
+                                        ')]</value>
+      <webElementGuid>f715a7d3-e1c5-4080-b545-39239b827872</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Portofolio Bugs/Page_Portfolio  PT Javan Cipta Solusi/button_Tipe Klien.rs
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Tipe Klien</name>
+   <tag></tag>
+   <elementGuidId>fdd0b31c-e5f9-48a8-8ff4-a4d008ddea08</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='portfolio-btn-jenis-sistem']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#portfolio-btn-jenis-sistem</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>2c68556b-581b-477b-9908-67bfb13762ec</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>portfolio-btn-jenis-sistem</value>
+      <webElementGuid>73ebb4ad-cfd7-4536-b8ba-5a765350623d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>0ade6704-fff2-4991-823c-98d4641482ac</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>inline-flex justify-between w-full py-4 px-4 border border-solid border-black-20% box-border rounded-md bg-white font-nunito font-semibold text-base text-black-50%</value>
+      <webElementGuid>54d24b3d-c92d-4d14-9793-4f825b0c0a74</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-haspopup</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>00a4de98-ce22-42a5-b6bf-f38ee7355e4f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-expanded</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>d0e29d3a-08a2-4d2b-b10a-78c9a3796178</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                            Tipe Klien
+                            
+                                
+                            
+                        </value>
+      <webElementGuid>46e1c102-e732-4d5f-a7a1-65ef787ef8b7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;portfolio-btn-jenis-sistem&quot;)</value>
+      <webElementGuid>6aed5492-c65c-404f-99eb-264c56f99fe5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@id='portfolio-btn-jenis-sistem']</value>
+      <webElementGuid>4a72844d-fc26-4b63-96b1-8936a11866ef</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tahun Pembuatan'])[1]/following::button[1]</value>
+      <webElementGuid>7b433b1e-0d92-421c-9544-33e171af8adb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Portfolio'])[4]/following::button[2]</value>
+      <webElementGuid>89fb5e0a-9a66-4ec5-b4c8-89f64717a5c7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='K/L (Kementerian/Lembaga) Pusat'])[1]/preceding::button[1]</value>
+      <webElementGuid>ad63ffba-88a9-4b57-ade6-7540f95d06f7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='K/L (Kementerian/Lembaga) Daerah'])[1]/preceding::button[1]</value>
+      <webElementGuid>e7f5f0e3-04dc-4f54-95c4-75e33aaf8ab7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Tipe Klien']/parent::*</value>
+      <webElementGuid>e5986271-ed02-40ae-a220-2054fe1b94fe</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[2]/div/button</value>
+      <webElementGuid>f45fb9e7-03b0-4f3e-aacb-1336a54b4483</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@id = 'portfolio-btn-jenis-sistem' and @type = 'button' and (text() = '
+                            Tipe Klien
+                            
+                                
+                            
+                        ' or . = '
+                            Tipe Klien
+                            
+                                
+                            
+                        ')]</value>
+      <webElementGuid>c43f0579-7077-42b7-a090-64252f1dd059</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
